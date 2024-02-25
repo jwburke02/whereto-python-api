d81c5544a7d4c4a9eed08cffef8b17e7be256a6c
John Burke <john@Johns-MacBook-Pro.local>
2024-02-24 19:26:46 -0500

    Added gatabase accessor file -- to implement

---
+++ services/db.py
@@ -0,0 +1,5 @@
+# A file to store interactions with our database: whereto.db
+
+# A function which places a data point into the database
+
+# A function which reads a list of data points from the data base in between a range (if any) [THEY MUST BE NOT TTK'ed]
