451247d32bc8d9f4263fcadd7c5c17e7c6bae9d4
John Burke <john@crc-dot1x-nat-10-239-167-231.bu.edu>
2024-03-06 13:43:15 -0500

    Added prediction of coordinates and confidence restrictions

--- services/ml.py
+++ services/ml.py
@@ -6,6 +6,7 @@ import io
 from core import model
 from multiprocessing.pool import ThreadPool
 from services.db import locationExists, getDetections, writeDetection, writeCoordinate
+import math
 
 def generate_base_heading(dy, dx):
     base_heading = math.atan2(dy, dx) * 180 / math.pi
@@ -23,7 +24,7 @@ def run_query(head_x_y):
     api = "&key=" + config.map_api_key
     location = "&location=" + str(x) + "," + str(y)
     try:
-        return [Image.open(io.BytesIO(requests.get("https://maps.googleapis.com/maps/api/streetview" + size + location + pitch + fov + "&heading=" + str(heading) + api).content)), "https://maps.googleapis.com/maps/api/streetview" + size + location + pitch + fov + "&heading=" + str(heading) + api]
+        return [Image.open(io.BytesIO(requests.get("https://maps.googleapis.com/maps/api/streetview" + size + location + pitch + fov + "&heading=" + str(heading) + api).content)), "https://maps.googleapis.com/maps/api/streetview" + size + location + pitch + fov + "&heading=" + str(heading) + api, head_x_y]
     except:
         return None
 
@@ -93,25 +94,22 @@ def run_model(street_coord_list):
                             result = results[0]
                             if len(result.boxes):
                                 print("Image Analyzed - Meter Found")
-                                conf = 0
-                                classifier = ""
-                                for box in result.boxes:
-                                    confidence = box.conf[0].item()
-                                    if confidence > conf:
-                                        conf = confidence
-                                        classifier = result.names[box.cls[0].item()]
-                                # y = street_coord_list[street][idx][1]
-                                # x = street_coord_list[street][idx][0]
-                                temp = {
-                                    "class_name": classifier,
-                                    "lat": x,
-                                    "lng": y,
-                                    "conf": confidence,
-                                    "image_url": im[1],
-                                    "text_read": None
-                                }
-                                writeDetection(temp, new_cid)
-                                locations[street]["detections"].append(temp)
+                                for box in result.boxes: # iterate through detections
+                                    # we use x (lat) and y (lng) + heading (im[2]['head']) to guess real placement of these objects
+                                    w = .00008 # some coordinate offset, about 30ish feet
+                                    guessed_lat = x + w * math.cos(im[2]['head'])
+                                    guessed_lng = y + w * math.sin(im[2]['head'])
+                                    temp = {
+                                        "class_name": result.names[box.cls[0].item()],
+                                        "lat": guessed_lat,
+                                        "lng": guessed_lng,
+                                        "conf": box.conf[0].item(),
+                                        "image_url": im[1],
+                                        "text_read": None
+                                    }
+                                    if box.conf[0].item() > .7: # only write if were confident
+                                        writeDetection(temp, new_cid)
+                                        locations[street]["detections"].append(temp)
                             else:
                                 print("Image Analyzed - Meter Not Found")
                                 continue
